39ee20a8109ff721b985ff1635fafeba1ec0636d
wymuxdev <wymuxgh@gmail.com>
2024-09-23 21:25:59 -0300

    change operators and start js

---
+++ assets/scripts/script.js
@@ -0,0 +1,5 @@
+const previousOperationText = document.querySelector('#previus-operation')
+const currentOperationText = document.querySelector('#current-operation')
+const buttons = document.querySelectorAll('main button')
+
+console.log(buttons)
