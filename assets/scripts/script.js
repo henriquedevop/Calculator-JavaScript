af163bb80844675c271d43599e63f6b2ae9bfb28
wymuxdev <wymuxgh@gmail.com>
2024-09-24 19:48:32 -0300

    delete javascript + take a break project

--- assets/scripts/script.js
+++
@@ -1,5 +0,0 @@
-const previousOperationText = document.querySelector('#previus-operation')
-const currentOperationText = document.querySelector('#current-operation')
-const buttons = document.querySelectorAll('main button')
-
-console.log(buttons)
